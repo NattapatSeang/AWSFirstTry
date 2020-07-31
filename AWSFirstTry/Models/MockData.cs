afee81f6c45af7fc279ab648b4217ec17503b379
NattapatSeang <51043893+NattapatSeang@users.noreply.github.com>
2020-07-31 12:07:24 +0700

    Create MockData.cs

---
+++ AWSFirstTry/Models/MockData.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AWSFirstTry.Models
+{
+    public class MockData
+    {
+        public int A { get; set; }
+        public string B { get; set; }
+        public string C { get; set; }
+        public string D { get; set; }
+        public string E { get; set; }
+        public string F { get; set; }
+        public string G { get; set; }
+        public string H { get; set; }
+    }
+}
